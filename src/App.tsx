1230cea1af644f26c74849634638450d7b2a570d
Stephen Cross <stephencross86@gmail.com>
2024-03-04 10:49:20 +0000

    Added select parameters and tidied example interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { Show, For, createSignal, Switch, Match } from 'solid-js';
 import Image from './components/Image';
 
+import { Select } from "@thisbeyond/solid-select";
+import "@thisbeyond/solid-select/style.css";
+
 import { debounce } from './lib/util';
 import { socketClient } from './lib/client';
 
@@ -31,7 +34,22 @@ function App() {
 
     const [loading, setLoading] = createSignal(true);
     const [source, setSource] = createSignal<string>();
-    const [params, setParams] = createSignal([{ "id": "", "name": "", "nickname": "", "canBeDisabled": "", "enabled": "", "visibleTitle": "", "parameters": [{ "name": "", "nickname": "", "value": "", "type": "" }] }]);
+    const [params, setParams] = createSignal([{
+        "id": "",
+        "name": "",
+        "nickname": "",
+        "canBeDisabled": "",
+        "enabled": "",
+        "visibleTitle": "",
+        "parameters":
+            [{
+                "name": "",
+                "nickname": "",
+                "value": "",
+                "type": "",
+                "choices": []
+            }]
+    }]);
 
     function process() {
         setLoading(true);
@@ -58,6 +76,10 @@ function App() {
         sendParameter(moduleID, parameterName, stringParameterValue);
     }
 
+    function sendChoiceParameter(moduleID: String, parameterName: String, value: String) {
+        sendParameter(moduleID, parameterName, value);
+    }
+
     function sendTextParameter(moduleID: String, parameterName: String, e: Event) {
         const stringParameterValue = (e.target as HTMLInputElement).value;
         sendParameter(moduleID, parameterName, stringParameterValue);
@@ -76,30 +98,37 @@ function App() {
                 <Image source={source()!} loading={loading()} />
             </Show>
 
-            <For each={params()}>{(module) =>
-                <div>
-                    {/* <h2><b>Module "{module.name}"</b></h2> */}
+            <table style="width:100%">
+                <For each={params()}>{(module) =>
                     <For each={module.parameters}>{(param) =>
-                        <div>
-                            <br />{param.nickname}:
-                            <Switch>
-                                <Match when={param.type === "BooleanP"}>
-                                    <input type="checkbox" id="fname" name="fname" checked={param.value === "true"} onInput={(e) => sendBooleanParameter(module.id, param.name, e)} /> {param.value}
-                                </Match>
-                                {/* <Match when={param.type === "FileFolderPathP"}>
+                        <tr>
+                            <td>
+                                {param.nickname}
+                            </td>
+                            <td>
+                                <Switch>
+                                    <Match when={param.type === "BooleanP"}>
+                                        <input type="checkbox" id="fname" name="fname" checked={param.value === "true"} onInput={(e) => sendBooleanParameter(module.id, param.name, e)} />
+                                    </Match>
+                                    <Match when={param.type === "ChoiceP"}>
+                                        {/* Only process if value is different */}
+                                        <Select options={param.choices} initialValue={param.value} onChange={(value: String) => value !== param.value ? sendChoiceParameter(module.id, param.name, value) : null} />
+                                    </Match>
+                                    {/* <Match when={param.type === "FileFolderPathP"}>
                                     
                                 </Match> */}
-                                <Match when={param.type === "DoubleP" || param.type == "IntegerP" || param.type == "StringP"}>
-                                    {/* <br/>{param.name}<input type="range" min="0" max="5" step="0.1" value={param.value} onChange={(e) => sendTextParameter(module.id,param.name,e)}/> {param.value} */}
-                                    <input type="text" id="fname" name="fname" value={param.value} onFocusOut={(e) => sendTextParameter(module.id, param.name, e)} />
-                                </Match>
-                            </Switch>
-                        </div>
+                                    <Match when={param.type === "DoubleP" || param.type == "IntegerP" || param.type == "StringP"}>
+                                        {/* <br/>{param.name}<input type="range" min="0" max="5" step="0.1" value={param.value} onChange={(e) => sendTextParameter(module.id,param.name,e)}/> {param.value} */}
+                                        <input type="text" id="fname" name="fname" value={param.value} onFocusOut={(e) => sendTextParameter(module.id, param.name, e)} style="text-align:center"/>
+                                    </Match>
+                                </Switch>
+                            </td>
+                        </tr>
                     }
                     </For>
-                </div>
-            }
-            </For>
+                }
+                </For>
+            </table>
 
         </main>
 
