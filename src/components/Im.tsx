ec456056fe55aa04c7e6d5e06b2e2cb8afa753cd
Stephen Cross <stephencross86@gmail.com>
2024-06-17 11:19:52 +0100

    Optimised brightness adjustment

--- src/components/Im.tsx
+++ src/components/Im.tsx
@@ -8,33 +8,14 @@ interface Props {
   loading?: boolean;
 }
 
-var times = [0]
-
-function addId(id:number, array:number[]) {
-  if (array.length === 100) {
-    array.pop();
-  }
-  array = array.splice(0, 0, id);
-  return array;
-}
-
-function averageTime(array: number[]) {
-  var sum = 0
-  array.forEach(element => {
-    sum = sum + element;
-  });
-  console.log (sum/100)
-}
-
 export default function Im(props: Props) {
   var compositeIm: CompositeImage
-  var imagedata: ImageData = null!
-  var lockIm = false;
+  var canvas: HTMLCanvasElement
+  var context: CanvasRenderingContext2D
   var currZoom = 1;
-  var currPan = {x:0,y:0};
+  var currPan = { x: 0, y: 0 };
 
   const [loading, setLoading] = createSignal(true);
-  const [error, setError] = createSignal(false);
   const [zoomControls, setZoomControls] = createSignal<PanzoomObject>();
 
   createEffect(
@@ -48,15 +29,26 @@ export default function Im(props: Props) {
           BrightnessStore.addNewValues(props.image.name, props.image.channels)
 
         setLoading(false);
-        imagedata = null!
-        compositeIm = new CompositeImage(props.image.channels);
-        (document.getElementById('currim') as HTMLImageElement).src = "data:image/png;base64," + compositeIm.getAsPNG();
 
-        const elem = document.getElementById('currim')
-        const panzoom = Panzoom(elem!, {maxScale: 10, contain: "outside", roundPixels: false})
+        canvas = (document.getElementById('image_canvas') as HTMLCanvasElement);
+        canvas.width = 512;
+        canvas.height = 512;
+        context = canvas.getContext("2d", { willReadFrequently: true })!;
+
+        if (context == undefined)
+          return;
+
+        context.clearRect(0, 0, canvas.width, canvas.height)
+
+        var new_im = new Image()
+        compositeIm = new CompositeImage(props.image.channels);
+        new_im.src = 'data:image/png;base64,' + compositeIm.getAsPNG()
+        new_im.onload = function () { context.drawImage(new_im, 0, 0) }
+
+        const panzoom = Panzoom(canvas!, { maxScale: 10, contain: "outside", roundPixels: false })
         panzoom.zoom(currZoom)
-        panzoom.pan(currPan.x,currPan.y)
-        elem?.parentElement?.addEventListener('click',updatePan)
+        panzoom.pan(currPan.x, currPan.y)
+        canvas?.parentElement?.addEventListener('click', updatePan)
         setZoomControls(panzoom)
 
       }
@@ -66,49 +58,21 @@ export default function Im(props: Props) {
   const hide = () => props.loading || loading();
 
   function updateBC(value: number, channel: number) {
-    if (lockIm)
-      return;
-
-    // var t1 = Date.now()
-    lockIm = true;
-
     BrightnessStore.updateValue(props.image.name, channel, value)
 
-    const currim = document.getElementById('currim') as HTMLImageElement;
-    if (currim == null)
-      return;
-
-    var canvas = document.createElement('canvas');
-    canvas.width = compositeIm.getWidth();
-    canvas.height = compositeIm.getHeight();
-
-    const context = canvas.getContext('2d');
-    context?.drawImage(currim, 0, 0);
-
-    if (imagedata == null)
-      imagedata = context?.getImageData(0, 0, compositeIm.getWidth(), compositeIm.getHeight())!;
-
+    var imagedata = context?.getImageData(0, 0, compositeIm.getWidth(), compositeIm.getHeight())!;
     if (imagedata == null)
       return;
 
     compositeIm.setChannelBrightness(imagedata, channel, value)
-    
     context?.putImageData(imagedata, 0, 0);
-    (document.getElementById('currim') as HTMLImageElement).src = canvas.toDataURL();
-
-    // var t2 = Date.now()
-    // addId(t2-t1,times)
-    // averageTime(times)
-
   }
 
   function updateZoom(event: HTMLInputElement) {
     // Setting zoom value
     var val = parseFloat(event.value)
     zoomControls()?.zoom(val);
-
     currZoom = val
-    
   }
 
   function updatePan() {
@@ -120,17 +84,7 @@ export default function Im(props: Props) {
   return (
     <div>
       <div>
-        <Show when={!error()} fallback={<p class="text-red-600">Error loading image</p>}>
-          <img
-            id="currim"
-            style="image-rendering: pixelated"
-            onLoad={() => { setLoading(false); lockIm = false; }}
-            alt="image"
-            classList={{ 'opacity-10': hide() }}
-            class="transition-opacity"
-            onError={() => setError(true)}
-          />
-        </Show>
+        <canvas id="image_canvas" width={512} height={512}></canvas>
       </div>
       <div>
         <Show when={props.image.showcontrols}>
