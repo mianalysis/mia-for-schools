fbdf577cea0179c2811fa99310c7ea91bfc747dc
Stephen Cross <stephencross86@gmail.com>
2026-03-24 12:29:18 +0000

    Changed how index.html is called

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,12 @@ import { render } from 'solid-js/web';
 import App from './App';
 import './styles/index.css';
 
+await new Promise<void>((resolve) => {
+  if ((window as any).cj) {
+    resolve();
+  } else {
+    window.addEventListener('cheerpj-ready', () => resolve(), { once: true });
+  }
+});
+
 render(() => <App />, document.getElementById('root'));
