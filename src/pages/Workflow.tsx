0362216e181ebc5c1a97e43543692bb57e5b6548
Stephen Cross <stephencross86@gmail.com>
2026-04-02 09:58:08 +0100

    Added real progress bar (may need calibrating)

--- src/pages/Workflow.tsx
+++ src/pages/Workflow.tsx
@@ -29,6 +29,11 @@ const [graph, setGraph] = createSignal<GraphJSON | undefined>();
 const [showNav, setShowNav] = createSignal(false);
 const [overlays, setOverlays] = createSignal<[OverlayJSON] | undefined>();
 const [clickListener, setClickListener] = createSignal<ClickListener | undefined>();
+const [loadedBytes, setLoadedBytes] = createSignal(0);
+
+window.setLoadedBytes = setLoadedBytes;
+const startingBytes = window.localLoadedBytes;
+const finalBytes = window.TOTAL_INIT_BYTES;
 
 // function requestHasPreviousGroup() {
 //   socketClient.publish({
@@ -247,22 +252,20 @@ function App() {
 
       <Show when={loading()}>
         <div
-          class="flex rounded-lg shadow-lg bg-white p-4 fade-in fade-out duration-1000 ease-in-out"
+          class="rounded-lg shadow-lg bg-white p-4 fade-in fade-out duration-1000 ease-in-out w-64"
           style="backdrop-filter: blur(6px); background-color: rgba(255,255,255,0.75); z-index: 1"
         >
-          <div role="status" class="animate-spin">
-            <svg class="w-12 h-12 text-neutral-tertiary" viewBox="0 0 100 101">
-              <path
-                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                fill="#BBBBBB"
-              />
-              <path
-                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                fill="#ff0000"
-              />
-            </svg>
+          <div class="text-2xl">Loading...</div>
+          <div class="w-full bg-neutral-quaternary rounded-full">
+            <div
+              class="mt-4 bg-violet-500 text-xs font-medium text-white text-center p-0.5 leading-none rounded-full h-4 flex items-center justify-center"
+              style={
+                'width: ' +
+                Math.min(100, (100 * (loadedBytes() - startingBytes)) / finalBytes) +
+                '%'
+              }
+            ></div>
           </div>
-          <div class="text-gray-600 ml-4 p-2 text-2xl">Loading...</div>
         </div>
       </Show>
 
